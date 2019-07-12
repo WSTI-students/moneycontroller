6fedfd0bb6d219c142b69e5dd692272d63b3a696
AnastasiiaZah <anastasiia.zahorodnia@gmail.com>
2019-07-12 19:49:10 +0200

    devise update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_182617) do
+ActiveRecord::Schema.define(version: 2019_07_12_172644) do
 
   create_table "balances", force: :cascade do |t|
     t.integer "value", default: 0, null: false
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 2019_07_11_182617) do
     t.integer "balance_id"
     t.index ["balance_id"], name: "index_incomes_on_balance_id"
     t.index ["category_id"], name: "index_incomes_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
