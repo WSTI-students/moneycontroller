cd026235349bb629828ad5b618052b2ecb2f4243
Anastasiia Zahorodnia <32658736+AnastasiiaZah@users.noreply.github.com>
2019-07-12 18:09:10 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
-  resources :expenses
-  resources :incomes
+  resources :expenses, only [:show, :new]
+  resources :incomes, only [:show, :new]
 end
