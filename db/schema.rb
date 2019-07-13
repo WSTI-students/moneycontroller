afa22bc6d5b7a2825367a2889e2223c8d19be298
AnastasiiaZah <anastasiia.zahorodnia@gmail.com>
2019-07-13 10:49:17 +0200

    migration_added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_061217) do
+ActiveRecord::Schema.define(version: 2019_07_13_083618) do
 
   create_table "balances", force: :cascade do |t|
     t.integer "value", default: 0, null: false
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_07_13_061217) do
     t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
   end
 
   create_table "categories", force: :cascade do |t|
