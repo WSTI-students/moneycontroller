d3280d2f5d3a142174b3ed375b837639a22010d2
AnastasiiaZah <anastasiia.zahorodnia@gmail.com>
2019-07-13 08:38:13 +0200

    Budget model created, added expenses form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,11 @@ class PagesController < ApplicationController
 
   def index
     @incomes = Income.all
-    current_balance_value = Balance.last.value
+    @expenses = Expense.all
+    @expenses_categories = Category.joins(:expenses).group('title').sum('expenses.value')
     current_expenses_value = Expense.sum(:value)
-    assets_left = current_balance_value - current_expenses_value
-    @current_balance = { 'Assets left' => assets_left, 'Expenses' => current_expenses_value }
+    current_budget_value = Budget.last.value
+    assets_left = current_budget_value - current_expenses_value
+    @current_budget = { 'Assets left' => assets_left, 'Expenses' => current_expenses_value }
   end
 end
