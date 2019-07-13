db72b6d0584264283684b2e39e239efcd6dbcdf5
AnastasiiaZah <anastasiia.zahorodnia@gmail.com>
2019-07-13 12:15:32 +0200

    budget-views

---
+++ app/controllers/budgets_controller.rb
@@ -0,0 +1,31 @@
+class BudgetsController < ApplicationController
+  def index; end
+
+  def new
+    @budget = current_user.budgets.new
+  end
+
+  def create
+    @budget = current_user.budgets.new(budget_params)
+
+    respond_to do |format|
+      if @budget.save
+        format.html { redirect_to root_path, notice: 'Budget was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit; end
+
+  private
+
+  def set_budget
+    @budget = Budget.find(params[:id])
+  end
+
+  def budget_params
+    params.require(:budget).permit(:title, :value)
+  end
+end
