be31911424e2969d3a8726dbcd9680622ad03cc5
AnastasiiaZah <anastasiia.zahorodnia@gmail.com>
2019-07-14 10:00:09 +0200

    Expenses_pie_chart_fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,8 @@ class PagesController < ApplicationController
   def index
     @incomes = Income.first(5)
     @expenses = Expense.first(5)
-    @expenses_categories = Category.joins(:expenses).group('title').sum('expenses.value_cents')
+    expenses_categories = Category.joins(:expenses).group('title').sum('expenses.value_cents')
+    @expense_pie = expenses_categories.transform_values { |v| v / 100.to_f }
     current_expenses_value = Expense.sum(:value_cents)
     current_budget_value = current_user.budgets.last.value_cents
     assets_left = (current_budget_value - current_expenses_value) / 100.to_f
