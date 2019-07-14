764ed0ffca48ad78a4dce11efba054067cf5f1ec
jerryskye <jarek@siedlarz.com>
2019-07-14 08:50:42 +0200

    Fix budget page and add welcome action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,15 @@
 class PagesController < ApplicationController
-  def index; end
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @incomes = Income.all
+    current_balance_value = Balance.last.value
+    current_expenses_value = Expense.sum(:value)
+    assets_left = current_balance_value - current_expenses_value
+    @current_balance = { 'Assets left' => assets_left, 'Expenses' => current_expenses_value }
+  end
+
+  def welcome
+    redirect_to action: 'index' if user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  root to: 'pages#welcome'
+  get '/budget', to: 'pages#index'
 
   resources :expenses, only: [:show, :new, :edit, :destroy]
   resources :incomes, only: [:show, :new, :edit, :destroy]
