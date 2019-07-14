1ee91a7d95341ee87717d93eca92ed951cf05f23
AnastasiiaZah <anastasiia.zahorodnia@gmail.com>
2019-07-14 09:56:54 +0200

    make_balance_expense_optional

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,11 +1,11 @@
 class Expense < ApplicationRecord
   belongs_to :category
-  belongs_to :balance
+  belongs_to :balance, optional: true
   belongs_to :budget
 
   validates :title, presence: true
   validates :value_cents, presence: true,
-                    numericality: { greater_than: 0 }
+                          numericality: { greater_than: 0 }
 
   monetize :value_cents
 end
